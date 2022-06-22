84bde3b0dfe86c73e62003172e56107b9eeb06b5
erika91mtz <erikagusto19@gmail.com>
2022-06-22 17:13:22 -0600

    updated city

--- src/script.js
+++ src/script.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "094ef7b80999d61ff577761824d67d3d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
