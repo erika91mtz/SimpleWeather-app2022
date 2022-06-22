91a2a9f2c1cf77db8830cadf777d6388235aded0
erika91mtz <erikagusto19@gmail.com>
2022-06-22 16:46:35 -0600

    js wind and humidity

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data.main.humidity);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "094ef7b80999d61ff577761824d67d3d";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
